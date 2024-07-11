5a8548cc9aa818cdee1aa54c514fc522dc5d2247
Youssef Bazzi <yoyobazzi3@gmail.com>
2024-07-11 11:35:51 -0700

    first commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ThreeDMenu from './components/ThreeDMenu';
+
+function App() {
+  return (
+      <div className="App">
+        <ThreeDMenu />
+      </div>
+    );
+}
+
+export default App;
---
+++ my-react-app/src/components/ThreeDMenu.js
@@ -0,0 +1,10 @@
+// ThreeDMenu.js
+import React, { useRef } from 'react';
+
+function Box(props) {
+    return(
+  <h1>Hello wold</h1>
+    );
+ 
+}
+export default Box
